6714a740083051efeba3f12d051817a28054c085
wettestsock <evilsharkeater@gmail.com>
2023-11-01 11:39:26 -0400

    h

---
+++ cpp/romtoi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+/*
+ROMAN INTEGER TO NUMBER
+
+if curr < next then substract
+if curr > next then add
+
+g++ romtoi.cpp -o out\romtoi
+out\romtoi
+
+g++ romtoi.cpp -o linout/romtoi
+linout/romtoi
+
+*/
+class Solution{
+public:
+    static unsigned int rom_to_int(std::string input){
+        unsigned int result = 0;
+
+
+
+        return result;
+    }
+
+};  
+
+int main(){
+    std::cout << Solution::rom_to_int("CMXCVIII")<<'\n';
+
+
+}
