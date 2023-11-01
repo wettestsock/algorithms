a6c6f74d9f7ee9de22f74cd3525a139fa9092fe3
wettestsock <evilsharkeater@gmail.com>
2023-11-01 11:23:05 -0400

    good morning

--- python/romtoi.py
+++ python/romtoi.py
@@ -2,4 +2,15 @@
 '''
 ROMAN TO INTEGER
 
+roman numerals normally written left to right, largest to smallest order
+7 symbols - I V X L C D M
+
+if adding smaller value after the larger -
+then substracting it
+cant put multiple smaller values before larger
+
+
+ex IM <- -1 and 1000 = 999
+
+
 '''
