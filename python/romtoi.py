1d0f6ca02695db9a61b439f17b0b51a6af515d4e
wettestsock <evilsharkeater@gmail.com>
2023-11-01 11:28:51 -0400

    ydfsuiyfis

--- python/romtoi.py
+++ python/romtoi.py
@@ -13,4 +13,20 @@ cant put multiple smaller values before larger
 ex IM <- -1 and 1000 = 999
 
 
+ex 2 :
+
+CMXCVIII
+
+is there a bigger value after it? then it's negative
+C < M then -100
+M > X then +1000
+X < C then -10
+C > V then +100
+V > I then +5
+I = I then +1
+I = I then +1
+I = I then +1
+
+ = 998
+
 '''
