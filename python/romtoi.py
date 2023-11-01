3b56b2458543c7183c53059ef0f86c111d9fb271
wettestsock <evilsharkeater@gmail.com>
2023-11-01 11:34:46 -0400

    hfejkh

--- python/romtoi.py
+++ python/romtoi.py
@@ -30,3 +30,19 @@ I = I then +1
  = 998
 
 '''
+#if i+1 < boundary    #checks if there's a num after the current
+
+class Solution:
+    def romanToInt(self, s:str) -> int:
+        # next is bigger then add
+        # next is smaller than substract
+        ROMAN = {'I':1, 'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+        output = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and ROMAN[s[i]] < ROMAN[s[i+1]]: 
+                output -= ROMAN[s[i]] # if next is bigger and if there's even a num after the num
+            else:
+                output += ROMAN[s[i]]
+        return output
+print(Solution.romanToInt(Solution, 'CMXCVIII'))
