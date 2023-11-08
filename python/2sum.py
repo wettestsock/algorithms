6dc1a5b85059b18d26b4538a6b006dde2ae544e2
wettestsock <evilsharkeater@gmail.com>
2023-11-08 11:56:14 -0500

    good mornig

---
+++ python/2sum.py
@@ -0,0 +1,11 @@
+'''
+
+MOST POPULAR LEETCODE PROBLEM
+2SUM
+
+given an array of ints, return indices of 2 numbers that add to a number
+
+theres always 1 solution
+check every combination of 2 values and see if they sum to the target
+
+'''
