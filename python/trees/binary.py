406c42eb03f5e9909c626cca955dbc3c961ce473
wettestsock <evilsharkeater@gmail.com>
2023-11-21 11:39:01 -0500

    sjhckskf

---
+++ python/trees/binary.py
@@ -0,0 +1,14 @@
+import numpy as npy 
+import pandas as pd
+
+'''
+INTRO TO BINARY TREES 
+TODO: implement in c / cpp
+
+always a root node 
+every left node gets smaller whereas every right node gets bigger
+
+
+BINARY SEARCH TREE 
+TODO: implement a binary search tree
+'''
