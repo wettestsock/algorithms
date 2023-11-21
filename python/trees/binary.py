ff24e46d70e8faa446919be9fc41b01747497144
wettestsock <evilsharkeater@gmail.com>
2023-11-21 11:54:05 -0500

    fjsdflk

--- python/trees/binary.py
+++ python/trees/binary.py
@@ -6,8 +6,10 @@ INTRO TO BINARY TREES
 TODO: implement in c / cpp
 
 always a root node 
-every left node gets smaller whereas every right node gets bigger
+every left node gets smaller whereas every right node gets bigger than their parent
 
+left is smaller
+right is bigger
 
 BINARY SEARCH TREE 
 TODO: implement a binary search tree (BST algorithm)
@@ -23,7 +25,19 @@ class node:
         #basically linked list
 
     def insert(self,data):
-        if(self.data is None):
-            self.data = data
+        if(self.data is None): # if node itself is null
+            self.data = data 
+            return
+        
+        #insert to the left 
+        if data < self.data:
+            if self.left is not None: # if theres nothing then put it
+                #if theres something there then recursively insert it there
+                self.left.insert(data) #gonna keep recursing until the condition becomes false
+
+            self.left = node(data) #new node using the data
+
+            #NOTE: tutorial implements as if and else statements
             
+                    
 
