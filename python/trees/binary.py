a07de1e15fe2af4309276c5c28df2231361cc264
wettestsock <evilsharkeater@gmail.com>
2023-11-21 12:20:25 -0500

    dskjfkls

--- python/trees/binary.py
+++ python/trees/binary.py
@@ -8,11 +8,24 @@ TODO: implement in c / cpp
 always a root node 
 every left node gets smaller whereas every right node gets bigger than their parent
 
+
+makes sure the bianry tree is sorted
 left is smaller
 right is bigger
 
 BINARY SEARCH TREE 
 TODO: implement a binary search tree (BST algorithm)
+
+
+4 ways to traverse a binary tree:
+
+1- depth first aproach of DFS
+    a. inorder traversal
+    b. preorder traversal
+    c. postorder traversal
+2- level order traversal or breadth first search of BFS
+3- boundary traversal
+4- diagonal traversal
 '''
 
 # node itself
@@ -27,17 +40,64 @@ class node:
     def insert(self,data):
         if(self.data is None): # if node itself is null
             self.data = data 
-            return
-        
-        #insert to the left 
-        if data < self.data:
-            if self.left is not None: # if theres nothing then put it
-                #if theres something there then recursively insert it there
-                self.left.insert(data) #gonna keep recursing until the condition becomes false
-
-            self.left = node(data) #new node using the data
-
-            #NOTE: tutorial implements as if and else statements
             
-                    
+        else:
+            if data < self.data: #inser to the left
+                if self.left is None: # if theres nothing then put it
+                    self.left = node(data) #new node using the data
+                    #NOTE: tutorial implements as if and else statements
+                else:
+                    #if theres something there then recursively insert it there
+                    self.left.insert(data) #gonna keep recursing until the condition becomes false
 
+            elif data > self.data:
+                # same as left but to right
+                if self.right is None:
+                    self.right = node(data)
+                
+                self.right.insert(data) #recursiveee
+
+        
+
+# printing all nodes InOrder
+
+'''
+lets say a tree like this
+
+        b (root)
+       / \
+(left)a   c  (right)
+       \
+        e
+
+starts at root, prints left, then prints the current, then prints the right
+in ex - prints a e b c 
+
+
+'''
+def in_order_print(curr:node):
+    if curr is None:
+        return
+
+    in_order_print(curr.left)
+    print(curr.data, end = ' ')
+    in_order_print(curr.right)
+
+# makes sure that the python file is NOT imported
+if __name__ == '__main__':
+    root = node('g')
+    root.insert('c')
+    root.insert('b')
+    root.insert('a')
+    root.insert('e')
+    root.insert('d')
+    root.insert('d')
+    root.insert('d')
+    root.insert('z')
+    root.insert('f')
+    root.insert('i')
+    root.insert('h')
+    root.insert('j')
+    root.insert('k')
+
+    in_order_print(root)
