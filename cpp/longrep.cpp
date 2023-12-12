990f3a721ba912ba13ff3d0d92f14773151bf727
wettestsock <evilsharkeater@gmail.com>
2023-12-12 11:53:14 -0500

    fgdjgpsd;

---
+++ cpp/longrep.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+/*
+LONGEST SUBSTRING WITHOUT REPEATING CHARACTERS
+
+SLIDING WINDOW TECHNIQUE
+with a set
+
+linout/longrep
+*/
+
+class Solution {
+public:
+    static int lengthOfLongestSubstring(const std::string& s) {
+        
+
+
+    }
+};
