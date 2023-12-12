adc7bdc31bfa5289ed804c5905196c392dda05bd
wettestsock <evilsharkeater@gmail.com>
2023-12-12 11:06:20 -0500

    good morning

---
+++ cpp/2sum.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+/*
+2 SUM
+2 NUMBERS THAT ADD TO THE TARGET
+
+
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(const vector<int>& nums, const int& target) {
+        for(int i=0; i<nums.len){
+        int diff = target;
+        
+
+        }
+        
+    }
+};
